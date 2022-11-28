9b4e1ea7837d108019868a72aa6c287af965d5c0
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2022-11-28 13:12:35 -0300

    uptade

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -2,20 +2,35 @@ function clicou(){
     let altura = document.getElementById('altura');
     let peso = document.getElementById('peso');
     let res = document.getElementById('res');
+    let sexo = document.getElementsByName('sex');
     let num1 = Number(altura.value);
     let num2 = Number(peso.value);
     let imc = num2 / num1 ** 2
     let total = imc.toFixed(2)
 
-    res.innerHTML = `Seu IMC é: ${total} <br> Menor que 18.5 - Abaixo do peso ;
-    <br>
-    Entre 18.5 e 24.9 - Peso normal ;
-    <br>
-    Entre 25.0 e 29.9 - Pré-obesidade ;
-    <br>
-    Entre 30.0 e 34.9 - Obesidade Grau 1 ;
-    <br>
-    Entre 35.0 e 39.9 - Obesidade Grau 2 ;
-    <br>
-    Acima de 40 - Obesidade Grau 3`
+    if(sexo[0].checked){
+        res.innerHTML = `Você é um homem e  seu IMC é: ${total}<br>
+        -Abaixo do peso: < 20.7
+        <br>
+        -Peso normal: 20.7 - 26.4
+        <br>
+        -Marginalmente acima do peso: 26.4 - 27.8
+        <br>
+        -Acima do peso ideal: 27.8 - 31.1 
+        <br> 
+        -Obeso: > 31.1
+        `
+    } else if(sexo[1].checked){
+        res.innerHTML = `Você é uma mulher e  seu IMC é: ${total}<br>
+        -Abaixo do peso: < 19.1
+        <br>
+        -Peso normal: 19.1 - 25.8
+        <br>
+        -Marginalmente acima do peso: 25.8 - 27.3
+        <br>
+        -Acima do peso ideal: 27.3 - 32.3
+        <br> 
+        -Obeso: > 32.3
+        `
+    }
 }
